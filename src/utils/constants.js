9d2ab526a8c5fcc03ff2707d889cd299e6ae4792
Aniket Tamrakar <anikettamrakar0@gmail.com>
2024-05-24 00:08:05 +0545

    removed secret key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,5 +14,3 @@ export const API_OPTIONS = {
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500"
-
-export const OPENAI_KEY = "sk-proj-AgWM6o73eGvYtPzd28RyT3BlbkFJKa90ut1S4wkuXUGDR9lX"
